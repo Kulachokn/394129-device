68cc8164b4b77bb388b613a37c675c11f3190721
Anastasiia Kulachok <kulachokn@gmail.com>
2019-01-18 18:25:16 +0200

    работа с первым слайдером общий рефакторинг

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@
 var writeUsBtn = document.querySelector('.write-us.button-decoration');
 var writeUsPopup = document.querySelector('.modal.modal-write-us');
 var closeWriteUs = document.querySelector('.close-write-us');
-var closeMap = document.querySelector('.close-map');
-var mapPopup = document.querySelector('.modal.modal-map');
-var map = document.querySelector('.map');
+// var closeMap = document.querySelector('.close-map');
+// var mapPopup = document.querySelector('.modal.modal-map');
+// var map = document.querySelector('.map');
 
-map.onclick = function(event) {
-  mapPopup.classList.remove('hide');
-  mapPopup.classList.add('animate-bounce');
-};
+// map.onclick = function(event) {
+//   mapPopup.classList.remove('hide');
+//   mapPopup.classList.add('animate-bounce');
+// };
 
 writeUsBtn.onclick = function (event) {
   event.preventDefault();
@@ -22,11 +22,11 @@ closeWriteUs.onclick = function (event) {
   writeUsPopup.classList.add('hide');
   writeUsPopup.classList.remove('animate-bounce');
 };
-
-closeMap.onclick = function (event) {
-  mapPopup.classList.add('hide');
-  mapPopup.classList.remove('animate-bounce');
-};
+//
+// closeMap.onclick = function (event) {
+//   mapPopup.classList.add('hide');
+//   mapPopup.classList.remove('animate-bounce');
+// };
 
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
@@ -34,8 +34,8 @@ window.addEventListener('keydown', function (evt) {
     if (!writeUsBtn.classList.contains('hide')) {
       writeUsPopup.classList.add('hide');
     }
-    if (!mapPopup.classList.contains('hide')) {
-      mapPopup.classList.add('hide');
-    }
+    // if (!mapPopup.classList.contains('hide')) {
+    //   mapPopup.classList.add('hide');
+    // }
   }
 });
